bbcddf325686463d26762bec884aed4b20a92ed9
Mario <mario.jayakumar@gmail.com>
2020-11-29 21:15:34 -0500

    finished PopularityANN, small bugs

--- test_popularity_ann.py
+++ test_popularity_ann.py
@@ -1,7 +1,15 @@
 from popularity_ann import PopularityANN, generate_sparse_connection_matrix
+import numpy as np
 
-
-
-f = PopularityANN(N=400, c=50)
+full_connection_mat = np.ones(shape=(5,5)) - np.eye(5)
+f = PopularityANN(N=5, c=3, connectivity_matrix=full_connection_mat)
 print(f.connections)
 print(f.C)
+
+data = [[1,-1,-1,-1,-1],[1,1,1,-1,-1]]
+data = np.array(data)
+labels = ["e1", "e2"]
+
+f.learn(data, labels)
+print(f.W)
+print(f.simulate(np.array([1,-1 ,1 ,-1, -1]), beta=1, threshold=0.5))
